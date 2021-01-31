70788a1cb6c59da4c2eabe8320d67a2ed080620b
Pedro Jesús Bazó Canelón <bazo.pedro@gmail.com>
2021-01-31 10:25:14 -0400

    Crear sistema autenticación

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Historia Clínica App
                 </div>
 
                 <div class="links">
